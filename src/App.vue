8e755a4a775f0edec28bfe43291551688536a2a7
Chase Ries <hello@chaseries.com>
2019-08-17 15:59:56 -0400

    Housekeeping

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,15 @@
 <template>
   <div id="app">
-    <layout-header></layout-header>
     <router-view></router-view>
-    <layout-footer></layout-footer>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import LayoutHeader from "VUE_COMPONENT/layout/header/LayoutHeader.vue";
-import LayoutFooter from "VUE_COMPONENT/layout/footer/LayoutFooter.vue";
 
 
 export default Vue.extend({
-  name: "app",
-  components: {
-    LayoutHeader,
-    LayoutFooter
-  }
+  name: "app"
 });
 </script>
 
